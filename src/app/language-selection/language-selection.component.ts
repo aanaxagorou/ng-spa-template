aa0ba9059485e51a5c2682e99a079f98f2462f3f
aanaxagorou <aanaxagorou@trading-point.com>
2024-08-14 23:55:01 +0300

    added font and font-awesome

--- src/app/language-selection/language-selection.component.ts
+++ src/app/language-selection/language-selection.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateService } from './translate.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-language-selection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './language-selection.component.html'
 })
 export class LanguageSelectionComponent {
